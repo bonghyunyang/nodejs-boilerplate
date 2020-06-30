8deb6724b998309aee3105c6c484b13a86322f24
bonghyunyang <bonghyunyang2@gmail.com>
2020-06-30 16:32:24 +0900

    강의 끝_ 로그아웃만 수정 필요

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 
 const userSchema = mongoose.Schema({
@@ -32,7 +35,65 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save', function( next ){
+    var user = this;
+    //비밀번호를 암호화시킨다.
+
+    if(user.isModified('password')) {
+
+    
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err)
+
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if(err) return next(err)
+            user.password = hash
+            next()
+        })
+    })
+   } else {
+       next()
+   }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+    //plainPassword = 암호화된 비밀번호 체크필요
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    // user._id + 'secretToken' = token
+
+    // -> 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function (token, cb){
+    var user = this;
+    //토큰을 디코드한다. 
+    jwt.verify(token, 'secretToken', function (err, decoded){
+        //유저아이디를 이용해서 유저를 찾은 다음에
+        //클라이언트에서 가져온 토큰과 디비에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id": decoded, "token": token }, function (err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User } //다른곳에서도 스키마쓸 수 있게 export 처리
-
